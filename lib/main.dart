b9346b0a2bc28011aa050878dd71004f560f95da
Royyan Zaydan <77264514+Kasehito@users.noreply.github.com>
2025-03-07 21:23:34 +0700

    feat: Set up app theme
    - Added AppTheme with custom color scheme and typography
    - Updated main.dart to use custom theme
    - Configured theme with Inter font and button styles
    
    Co-authored-by: Royyan AZ <kasehitoworks@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:function_mobile/modules/auth/views/login_page.dart';
+import 'package:function_mobile/theme/theme_data.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +11,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      theme: AppTheme.themeData,
+      home: LoginPage(),
     );
   }
 }
