8d7b0a989dc93bb700a685388f8689807cc50299
Royyan Zaydan <77264514+Kasehito@users.noreply.github.com>
2025-03-07 22:03:18 +0700

    feat: Implement login page UI with custom components
    - Added custom text fields for email/username and password
    - Integrated PrimaryButton component
    - Styled login page with consistent layout and typography
    
    Co-authored-by: Royyan AZ <kasehitoworks@gmail.com>

--- lib/modules/auth/views/login_page.dart
+++ lib/modules/auth/views/login_page.dart
@@ -1,19 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:function_mobile/components/buttons/primary_button.dart';
+import 'package:function_mobile/components/inputs/custom_text_field.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
-  Widget build(context) {
-    return SafeArea(child: Scaffold(
-      body: Center(
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(16),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('Login Page'),
-            
-            ElevatedButton(onPressed: () {}, child: const Text('Login')),
-
+            const Text('Login',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+            SizedBox(height: 18),
+            Column(
+              children: [
+                Text("Email/Username",
+                    style: Theme.of(context).textTheme.bodyMedium),
+                SizedBox(height: 18),
+                CustomTextField(
+                  hintText: 'Enter your email or username',
+                  isPassword: false,
+                ),
+              ],
+            ),
+            SizedBox(height: 18),
+            Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+              Text("Password", style: Theme.of(context).textTheme.bodyMedium),
+              CustomTextField(
+                hintText: 'Enter your password',
+                isPassword: true,
+              ),
+            ]),
+            SizedBox(height: 18),
+            PrimaryButton(text: 'Login', onPressed: () {}),
           ],
         ),
       ),
