22c73c32dffe793d40af575955cd36921a59e102
Royyan Zaydan <77264514+Kasehito@users.noreply.github.com>
2025-03-08 01:14:57 +0700

    feat: Enhance login page UI with improved layout and additional features
    - Disabled debug banner in MaterialApp
    - Refactored login page to use SingleChildScrollView
    - Added password field and forgot password button
    - Included signup prompt and social login (Google) option
    - Improved text styling and layout spacing
    
    Co-authored-by: Royyan AZ <kasehitoworks@gmail.com>

--- lib/modules/auth/views/login_page.dart
+++ lib/modules/auth/views/login_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:function_mobile/components/buttons/primary_button.dart';
 import 'package:function_mobile/components/inputs/custom_text_field.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:function_mobile/components/buttons/outline_button.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -8,39 +10,90 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Login',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-            SizedBox(height: 18),
-            Column(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text("Email/Username",
-                    style: Theme.of(context).textTheme.bodyMedium),
+                Text('Login', style: Theme.of(context).textTheme.displaySmall),
                 SizedBox(height: 18),
-                CustomTextField(
-                  hintText: 'Enter your email or username',
-                  isPassword: false,
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Email/Username",
+                        style: Theme.of(context).textTheme.bodyLarge),
+                    SizedBox(height: 8),
+                    CustomTextField(
+                      hintText: 'Enter your email or username',
+                      isPassword: false,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 18),
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                  Text("Password",
+                      style: Theme.of(context).textTheme.bodyLarge),
+                  SizedBox(height: 8),
+                  CustomTextField(
+                    hintText: 'Enter your password',
+                    isPassword: true,
+                  ),
+                ]),
+                SizedBox(height: 18),
+                PrimaryButton(text: 'Login', onPressed: () {}),
+                SizedBox(height: 18),
+                TextButton(
+                    onPressed: () {}, child: const Text('Forgot Password?')),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Don't have an account?",
+                        style: TextStyle(fontSize: 16)),
+                    TextButton(
+                        onPressed: () {},
+                        child: const Text('Signup',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ))),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        color: Colors.grey[400],
+                        thickness: 2,
+                        height: 40,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 8),
+                      child: Text('Or Login With',
+                          style: Theme.of(context).textTheme.bodyMedium),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        color: Colors.grey[400],
+                        thickness: 2,
+                        height: 40,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 18),
+                OutlineButton(
+                  text: "Login dengan Google",
+                  icon: FontAwesomeIcons.google,
+                  useFaIcon: true,
+                  onPressed: () {},
                 ),
               ],
             ),
-            SizedBox(height: 18),
-            Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-              Text("Password", style: Theme.of(context).textTheme.bodyMedium),
-              CustomTextField(
-                hintText: 'Enter your password',
-                isPassword: true,
-              ),
-            ]),
-            SizedBox(height: 18),
-            PrimaryButton(text: 'Login', onPressed: () {}),
-          ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
