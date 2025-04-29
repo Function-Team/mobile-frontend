6a534fe3e34afa14254faa67cd43d04b5493531b
ForcesR2N <muhammadzuhrizal07@gmail.com>
2025-04-29 11:00:26 +0700

    feat : adding feature search for capacity v2
    - Mix controller code for feature search in file searchFilterController
    - Finnish code for feature search by capacity

--- lib/modules/home/widgets/search_container.dart
+++ lib/modules/home/widgets/search_container.dart
@@ -21,7 +21,7 @@ class SearchContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    SearchFilterController searchFilterController = Get.find();
+    final searchController = Get.find<SearchFilterController>();
 
     return Container(
       padding: EdgeInsets.all(16),
@@ -31,129 +31,132 @@ class SearchContainer extends StatelessWidget {
         borderRadius: BorderRadius.circular(12),
       ),
       child: Column(
-          children: [
-        // Activity/Venue Search Field
-        InkWell(
-          onTap: () {
-            searchFilterController.goToSearchActivity();
-          },
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey[300]!),
-              borderRadius: BorderRadius.circular(6),
-            ),
-            child: Row(
-              children: [
-                Icon(Icons.search, color: Colors.grey[600]),
-                SizedBox(width: 8),
-                Text(
-                  controllerActivity.text.isEmpty
-                      ? 'Search Activity/Venue'
-                      : controllerActivity.text,
-                  style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                        color: Colors.grey[600],
-                      ),
-                ),
-              ],
+        children: [
+          // Activity/Venue Search Field
+          InkWell(
+            onTap: () {
+              searchController.goToSearchActivity();
+            },
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey[300]!),
+                borderRadius: BorderRadius.circular(6),
+              ),
+              child: Row(
+                children: [
+                  Icon(Icons.search, color: Colors.grey[600]),
+                  SizedBox(width: 8),
+                  Text(
+                    controllerActivity.text.isEmpty
+                        ? 'Search Activity/Venue'
+                        : controllerActivity.text,
+                    style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                          color: Colors.grey[600],
+                        ),
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
 
-        // Location Search Field
-        InkWell(
-          onTap: () {
-            searchFilterController.goToSearchLocation();
-          },
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey[300]!),
-              borderRadius: BorderRadius.circular(6),
-            ),
-            child: Row(
-              children: [
-                Icon(Icons.location_on_outlined, color: Colors.grey[600]),
-                SizedBox(width: 8),
-                Text(
-                  controllerLocation.text.isEmpty
-                      ? 'Search Location'
-                      : controllerLocation.text,
-                  style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                        color: Colors.grey[600],
-                      ),
-                ),
-              ],
+          // Location Search Field
+          InkWell(
+            onTap: () {
+              searchController.goToSearchLocation();
+            },
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey[300]!),
+                borderRadius: BorderRadius.circular(6),
+              ),
+              child: Row(
+                children: [
+                  Icon(Icons.location_on_outlined, color: Colors.grey[600]),
+                  SizedBox(width: 8),
+                  Text(
+                    controllerLocation.text.isEmpty
+                        ? 'Search Location'
+                        : controllerLocation.text,
+                    style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                          color: Colors.grey[600],
+                        ),
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
 
-        // Capacity Selection
-        InkWell(
-          onTap: () {
-            searchFilterController.goToCapacitySelection();
-          },
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey[300]!),
-              borderRadius: BorderRadius.circular(6),
-            ),
-            child: Row(
-              children: [
-                Icon(Icons.people_outline, color: Colors.grey[600]),
-                SizedBox(width: 8),
-                Text(
-                  controllerCapacity.text.isEmpty
-                      ? 'Choose Capacity'
-                      : controllerCapacity.text,
-                  style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                        color: Colors.grey[600],
-                      ),
-                ),
-              ],
+          // Capacity Selection
+          InkWell(
+            onTap: () {
+              searchController.goToCapacitySelection();
+            },
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey[300]!),
+                borderRadius: BorderRadius.circular(6),
+              ),
+              child: Row(
+                children: [
+                  Icon(Icons.people_outline, color: Colors.grey[600]),
+                  SizedBox(width: 8),
+                  Text(
+                    controllerCapacity.text.isEmpty
+                        ? 'Choose Capacity'
+                        : controllerCapacity.text,
+                    style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                          color: Colors.grey[600],
+                        ),
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
 
-        // Date Selection
-        InkWell(
-          onTap: () {
-            searchFilterController.goToDateSelection();
-          },
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey[300]!),
-              borderRadius: BorderRadius.circular(6),
-            ),
-            child: Row(
-              children: [
-                Icon(Icons.calendar_today_outlined, color: Colors.grey[600]),
-                SizedBox(width: 8),
-                Text(
-                  controllerDate.text.isEmpty
-                      ? 'Choose Date'
-                      : controllerDate.text,
-                  style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                        color: Colors.grey[600],
-                      ),
-                ),
-              ],
+          // Date Selection
+          InkWell(
+            onTap: () {
+              // searchController.goToDateSelection();
+            },
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey[300]!),
+                borderRadius: BorderRadius.circular(6),
+              ),
+              child: Row(
+                children: [
+                  Icon(Icons.calendar_today_outlined, color: Colors.grey[600]),
+                  SizedBox(width: 8),
+                  Text(
+                    controllerDate.text.isEmpty
+                        ? 'Choose Date'
+                        : controllerDate.text,
+                    style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                          color: Colors.grey[600],
+                        ),
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-        SecondaryButton(
-          text: 'Search',
-          onPressed: onTapSearch,
-          width: double.infinity,
-        ),
-      ]
-              .map((e) => Padding(
-                    padding: const EdgeInsets.only(bottom: 10),
-                    child: e,
-                  ))
-              .toList()),
+          
+          // Search Button
+          SecondaryButton(
+            text: 'Search',
+            onPressed: onTapSearch,
+            width: double.infinity,
+          ),
+        ]
+        .map((e) => Padding(
+              padding: const EdgeInsets.only(bottom: 10),
+              child: e,
+            ))
+        .toList(),
+      ),
     );
   }
 }
