fd56604926aac51ea0e9462b626f49080f37e682
Royyan Ahmad Zaydan <05302@smkradenumarsaidkudus.sch.id>
2025-04-30 15:30:10 +0700

    feat: add secure storage, API service, and service bindings
    Introduce secure storage for token management, refactor API service to use Dio for better error handling, and add service bindings for dependency injection. Also, add network security config for Android and update dependencies.

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,15 @@
+class AppConstants {
+  // API URLs
+  static const String baseUrl = 'YOUR_BASE_URL';
+
+  // Route Names
+  static const String home = '/home';
+  static const String search = '/search';
+
+  // Asset Paths
+  static const String imagePath = 'assets/images/';
+
+  // Shared Preferences Keys
+  static const String tokenKey = 'auth_token';
+  static const String userKey = 'user_data';
+}
