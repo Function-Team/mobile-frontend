884602b412320a43ed6c8b26173e64608bb0ae7e
Royyan Ahmad Zaydan <05302@smkradenumarsaidkudus.sch.id>
2025-07-30 11:19:09 +0700

    refactor(api): switch from local to production base URL

--- lib/core/services/api_service.dart
+++ lib/core/services/api_service.dart
@@ -11,7 +11,7 @@ class ApiService extends GetxService {
 
   ApiService() {
     _dio = dio.Dio(dio.BaseOptions(
-      baseUrl: AppConstants.baseUrlLocal,
+      baseUrl: AppConstants.baseUrl,
       connectTimeout: const Duration(seconds: 10),
       receiveTimeout: const Duration(seconds: 10),
       sendTimeout: const Duration(seconds: 10),
